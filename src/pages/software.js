49da5ce0667488a25bbe4a77501e1f3222533c91
Emma Barnes <emma@snowbooks.com>
2019-08-22 19:26:05 +0100

    update images and typo

--- src/pages/software.js
+++ src/pages/software.js
@@ -10,13 +10,13 @@ export default () => (
     <div className="main-wrapper">
       <div className="wrapper">
         <div style={{ margin: '0 auto', padding: 20, maxWidth: 1100 }}>
-          <Heading node='h1'>Consonance: futuristic Publishing management software</Heading>
+          <Heading node='h1'>Consonance: futuristic publishing management software</Heading>
           <Paragraph>
             <Link
               className='success'
               href="https://consonance.app"
             >
-              Read all about our flagship, comprehensive publishing sofware on the Consonance website >
+              Read all about our flagship comprehensive publishing software on the Consonance website >
             </Link>
             </Paragraph>
 
